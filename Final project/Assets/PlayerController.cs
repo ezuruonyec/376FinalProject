0f6d8ca39d7ae8a2bc05d7a9f4002e42bcc01585
Hannah Cain <hcsch18@gmail.com>
2022-04-12 23:06:32 -0400

    added controlls

--- Final project/Assets/PlayerController.cs
+++ Final project/Assets/PlayerController.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Animator anim;
+    private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 rotation;
+    public float groundDistance = 0.0f;
+    public float JumpForce = 500;
+    public LayerMask isGround;
+
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) anim.SetTrigger("Walk");
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Speed", ver);
+        this.rotation = new Vector3(0, hor * 180 * Time.deltaTime, 0);
+        this.transform.Rotate(this.rotation);
+        if(Input.GetButtonDown("Jump")){
+            rigid.AddForce(Vector3.up * JumpForce);
+            anim.SetTrigger("Jump");
+        }
+        if(Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, groundDistance, isGround)){
+            anim.SetBool("Grounded", true);
+            anim.applyRootMotion = true;
+        } else {
+            anim.SetBool("Grounded", false);
+        }
+        
     }
 }
