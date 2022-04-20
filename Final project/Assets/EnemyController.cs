71fdf351ae25fd7aaee5f3368185347f676f0043
Hannah Cain <hcsch18@gmail.com>
2022-04-20 00:31:12 -0400

    player interactions done, added all enemy anims

---
+++ Final project/Assets/EnemyController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: AI 
+        // if player in area approach them 
+        //if player in range, attack them 
+        //add trigger collider to sword to attack player
+        //if hit by player sword, take damage
+        //if health <= 0, die
+        
+    }
+}
